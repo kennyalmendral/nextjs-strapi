3ff3bb56be13efaf64236ac9d88a022d4e33e8cd
kennyalmendral <kennyalmendral@gmail.com>
2021-06-06 21:26:27 +0800

    Add helpers

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,34 @@
+import { parseCookies } from '@/helpers/index'
+
 import Layout from '@/components/Layout'
 
-export default function Dashboard() {
+import { API_URL } from '@/config/index'
+
+export default function Dashboard({ events }) {
+  console.log(events)
+  
   return (
     <Layout title="Dashboard | Next.js + Strapi">
       <h1>Dashboard</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    }
+  }
 }
